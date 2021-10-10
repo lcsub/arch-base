8880cf756fbcb9c2a87f49fa3c53b8036f2002c6
lcsub <90230706+lcsub@users.noreply.github.com>
2021-10-10 13:14:05 -0300

    Add files via upload

---
+++ arch-install-base.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# config
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+hwclock --systohc
+sed -i '177s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
+echo "workstation" >> /etc/hostname
+echo "127.0.0.1	localhost" >> /etc/hosts
+echo "::1		localhost" >> /etc/hosts
+echo "127.0.1.1	workstation.localdomain workstation" >> /etc/hosts
+echo root:nnsd8r78 | chpasswd
+useradd -m lcsub
+echo lcsub:lyt56erw | chpasswd
+echo "lcsub ALL=(ALL) ALL" >> /etc/sudoers.d/lcsub
+pacman -Sy
+pacman -S --noconfirm efibootmgr grub xf86-video-amdgpu xorg-server os-prober qutebrowser xorg-xinit dhcpcd
+
+# xinit config
+cp /etc/X11/xinit/xinitrc /home/lcsub/.xinitrc
+
+# bootloader
+os-prober
+echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# service
+systemctl enable fstrim.timer
+systemctl enable dhcpcd@enp4s0
---
+++ grub.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub_uefi --recheck
+cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+grub-mkconfig -o /boot/grub/grub.cfg
